9c69b44aa64a029aeecefb06ef8528239fd14ca6
Alek Storm <alek.storm@gmail.com>
2011-10-29 12:36:30 -0400

    Allow user to pass arbitrary arguments to Closure Compiler

--- build/build.bash
+++ build/build.bash
@@ -18,7 +18,8 @@ if [ -e $FINAL ];then
 	rm $FINAL
 fi
 
-java -jar $1 \
+ARGS=("$@")
+java -jar $1 $2 \
     --js_output_file $OUTPUT \
     --js ../src/easeljs/utils/UID.js \
     --js ../src/easeljs/utils/Ticker.js \
